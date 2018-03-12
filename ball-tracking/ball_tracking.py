8a6c6e2504e075987b2342219fb78a256b81f29a
Ayodeji Marquis <ayodejimarquis@Ayodejis-MacBook-Pro.local>
2018-03-11 22:31:10 -0400

    soft and hard kill implemented

--- ball-tracking/ball_tracking.py
+++ ball-tracking/ball_tracking.py
@@ -15,20 +15,26 @@ ap.add_argument("-b", "--buffer", type=int, default=64,
                 help="max buffer size")
 args = vars(ap.parse_args())
 pts = deque(maxlen=args["buffer"])
-ballCount = 0
+LED_PAUSE = 17
+LED_ACTIVE = 22
+PAUSE_SWITCH = 4
+ntime, ballCount, switch = 0, 0, 0
 lowerColorBound = (29, 86, 6)
 upperColorBound = (64, 255, 255)
 # lowerColorBound = (29, 24, 6)
 # upperColorBound = (72, 255, 255)  # new camera improvements
 moveForward = False
-ntime = 0
 
 
 def setup():
     # LED setup
+    # 18 = white, 23 = green, 24 = blue
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    # 18 = white, 23 = green, 24 = blue
+    GPIO.setup(PAUSE_SWITCH, GPIO.IN)
+    GPIO.setup(LED_PAUSE, GPIO.OUT)
+    GPIO.setup(LED_ACTIVE, GPIO.OUT)
+    ########################################
     GPIO.setup(18, GPIO.OUT)
     GPIO.setup(23, GPIO.OUT)
     GPIO.setup(24, GPIO.OUT)
@@ -60,12 +66,6 @@ def goRight():
 
 # TODO
 def moveForwardABit():
-    # time_end = time.time() +tf
-    # while time.time() < time_end:
-    # GPIO.output(18, GPIO.LOW)
-    # GPIO.output(23, GPIO.HIGH)
-    # GPIO.output(24, GPIO.LOW)
-    # GPIO.output(27, GPIO.HIGH)
     global ntime
     ntime = time.time()
     print("moveforwardabt", ntime)
@@ -73,7 +73,6 @@ def moveForwardABit():
 
 # TODO
 def roomba():
-    # print("roomba")
     GPIO.output(18, GPIO.LOW)
     GPIO.output(23, GPIO.LOW)
     GPIO.output(24, GPIO.LOW)
@@ -87,113 +86,129 @@ def stop():
     GPIO.output(27, GPIO.LOW)
 
 
+def pause():
+    stop()
+    GPIO.output(LED_PAUSE, GPIO.HIGH)
+    GPIO.output(LED_ACTIVE, GPIO.LOW)
+    
+    
+def resume():
+    GPIO.output(LED_PAUSE, GPIO.LOW)
+    GPIO.output(LED_ACTIVE, GPIO.HIGH)
+
+
 def shutdown():
     camera.release()
     GPIO.output(18, GPIO.LOW)
     GPIO.output(23, GPIO.LOW)
     GPIO.output(24, GPIO.LOW)
-    GPIO.setup(27, GPIO.LOW)
+    GPIO.output(27, GPIO.LOW)
+    #########################
+    GPIO.output(LED_PAUSE, GPIO.LOW)
+    GPIO.output(LED_ACTIVE, GPIO.LOW)
     cv2.destroyAllWindows()
-
 
 setup()
 camera = cv2.VideoCapture(0)  # Capture Video from web cam...
 print("Camera warming up ...")
 
+
 while True:
-    (captured, frame) = camera.read()
-    if args.get("video") and not captured:
-        break
+    if GPIO.input(PAUSE_SWITCH):
+        switch += 1
+    elif switch % 2 == 1:
+        pause()
+    elif switch % 2 == 0:
+        resume()
+        (captured, frame) = camera.read()
+        if args.get("video") and not captured:
+            break
 
-    # resize the frame, and convert it to the HSV color space
-    width = 400  # 640 might be reasonable
-    frame = imutils.resize(frame, width)
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    cv2.imshow("HSV", hsv)
+        # resize the frame, and convert it to the HSV color space
+        width = 400  # 640 might be reasonable
+        frame = imutils.resize(frame, width)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color "yellowish/green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, lowerColorBound, upperColorBound)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-    mask_blur = cv2.GaussianBlur(mask, (15, 15), 0)
-    cv2.imshow("blurredmask", mask_blur)
-    # param1=50, param2=35/15
-    hough_circles = cv2.HoughCircles(mask_blur, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=25, minRadius=0,
-                                     maxRadius=0)
+        # construct a mask for the color "yellowish/green", then perform
+        # a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, lowerColorBound, upperColorBound)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        mask_blur = cv2.GaussianBlur(mask, (15, 15), 0)
+        cv2.imshow("blurredmask", mask_blur)
+        # param1=50, param2=35/15
+        # TODO - play with the params
+        hough_circles = cv2.HoughCircles(mask_blur, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=25, minRadius=0,
+                                         maxRadius=60)
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
-
-    if moveForward:
-        print("time!!!!!")
-        goForward()
-        if time.time() > ntime + .5:
-            print(ntime)
-            moveForward = False
-            stop()
-
-    # only proceed if at least one contour was found
-    elif len(cnts) > 0 and hough_circles is not None:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        cv2.circle(frame, center, 5, (255, 0, 0), -1)
-        # print('center: ', center, 'radius', int(radius))  # outputs coordinate to command line
-        leftBound = (width / 2) - (0.15 * width)
-        rightBound = (width / 2) + (0.15 * width)
-
-        if leftBound <= center[0] <= rightBound:
+        # find contours in the mask
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
+        if moveForward:
+            print("time!!!!!")
             goForward()
+            if time.time() > ntime + .5:
+                stop()
+                print(time.time())
+                ballCount = ballCount + 1
+                print('Ball Retrieved ' + str(ballCount))
+                moveForward = False
 
-        elif center[0] < leftBound:
-            goLeft()
+        # only proceed if at least one contour was found
+        elif len(cnts) > 0 and hough_circles is not None:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            cv2.circle(frame, center, 5, (255, 0, 0), -1)
+            print('center: ', center, 'radius', int(radius))  # outputs coordinate to command line
+            leftBound = (width / 2) - (0.15 * width)
+            rightBound = (width / 2) + (0.15 * width)
 
+            if leftBound <= center[0] <= rightBound:
+                goForward()
+
+            elif center[0] < leftBound:
+                goLeft()
+
+            else:
+                goRight()
+
+            # draw outer circle if the radius meets a minimum size
+            if radius > 0.05 * width:
+                # cv2.circle(image, center, radius, color, thickness)
+                cv2.circle(frame, (int(x), int(y)), int(radius),
+                           (0, 0, 255), 2)
+
+            if radius >= 0.1325 * width:
+                # ball is close enough to be retrieved!
+                print("2time!!!!!")
+                moveForwardABit()
+                moveForward = True
         else:
-            goRight()
+            roomba()
 
-        # draw outer circle if the radius meets a minimum size
-        if radius > 0.05 * width:
-            # cv2.circle(image, center, radius, color, thickness)
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                       (0, 0, 255), 2)
+        pts.appendleft(center)   # update the points queue
 
-        if radius >= 0.1325 * width:
-            # ball is close enough to be retrieved!
-            # TODO
-            # ballCount = ballCount + 1  # look into this!
-            # print('Ball Retrieved ' + str(ballCount))
-            print("2time!!!!!")
-            moveForwardABit()
-            moveForward = True
-    else:
-        roomba()
+        # loop over the set of tracked points
+        for i in xrange(1, len(pts)):
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+            # otherwise, compute the thickness of the line and draw the connecting lines
+            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            # cv.Line(img, pt1, pt2, color, thickness=1, lineType=8, shift=0)
+            cv2.line(frame, pts[i - 1], pts[i], (255, 0, 0), thickness)
 
-    # update the points queue
-    pts.appendleft(center)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-    # loop over the set of tracked points
-    for i in xrange(1, len(pts)):
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        # otherwise, compute the thickness of the line and draw the connecting lines
-        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        # cv.Line(img, pt1, pt2, color, thickness=1, lineType=8, shift=0)
-        cv2.line(frame, pts[i - 1], pts[i], (255, 0, 0), thickness)
-
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
+        # if the 'q' key is pressed, stop the loop
+        if key == ord("q"):
+            break
 
 shutdown()
